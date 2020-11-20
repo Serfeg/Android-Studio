812c0a76bfb47cf938fe6fe6083a4e26f125aa61
Serfeg <mirrorsnight@gmail.com>
2020-11-20 15:04:25 +0700

    Вывод цвета

--- MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         Log.i("GSON", "Имя: " + murzik.name + "\nВозраст: " + murzik.age);
 
         TextView cat_tv = findViewById(R.id.cat_tv);
-        cat_tv.setText("Имя: " + murzik.name + "\nВозраст: " + murzik.age);
+        cat_tv.setText("Имя: " + murzik.name + " Возраст: " + murzik.age + " Цвет: ");
+        View vColor = findViewById(R.id.vColor);
+        vColor.setBackgroundColor(murzik.color);
     }
 }
