ed622a4326d9bf97c00301ba143e60a705aef153
Serfeg <mirrorsnight@gmail.com>
2020-11-20 14:21:56 +0700

    Конвертация объета в JSON

--- MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.BLACK;
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
     }
 }
