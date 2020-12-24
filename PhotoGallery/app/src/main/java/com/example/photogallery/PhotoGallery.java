0469a4ac00e260e3439b9a9e441bf18a8c234d9b
Serfeg <mirrorsnight@gmail.com>
2020-12-24 13:29:17 +0700

    Добавление поиска

--- PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGallery.java
+++ PhotoGallery/app/src/main/java/com/example/photogallery/PhotoGallery.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.photogallery.api.FlickrAPI;
@@ -30,7 +33,7 @@ public class PhotoGallery extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_activity);
 
-        rView = findViewById(R.id.rView);
+        final RecyclerView rView = findViewById(R.id.rView);
         rView.setLayoutManager(new GridLayoutManager(this,3));
         context = this;
         Retrofit retrofit = ServiceAPI.getRetrofit();
@@ -48,5 +51,43 @@ public class PhotoGallery extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_view,menu);
+        MenuItem searchItem = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView)searchItem.getActionView();
+        rView = findViewById(R.id.rView);
+        rView.setLayoutManager(new GridLayoutManager(this,3));
+        context = this;
+        Retrofit retrofit = ServiceAPI.getRetrofit();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                retrofit.create(FlickrAPI.class).getSearchPhotos(query).enqueue(new Callback<Response>() {
+                    @Override
+                    public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                        resp = response.body();
+                        List<Photo> ph = resp.getPhotos().getPhoto();
+                        adapter = new PhotoAdapter(ph,context);
+                        rView.setAdapter(adapter);
+                        Toast.makeText(PhotoGallery.this, "GOOD REQUEST",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Response> call, Throwable t) {
+                        Toast.makeText(PhotoGallery.this, "BAD REQUEST",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickrAPI.java
+++ PhotoGallery/app/src/main/java/com/example/photogallery/api/FlickrAPI.java
@@ -7,9 +7,9 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface FlickrAPI {
-    @GET("services/rest/?method=flickr.photos.getRecent&api_key=6a35b2891167a106d427815ad7378f25&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1")
+    @GET("services/rest/?method=flickr.photos.getRecent&api_key=1a34bdcc540e105b49dac5f7e270bf4f&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1")
     Call<Response> getRecent();
-    @GET("services/rest/?method=flickr.photos.getRecent&api_key=6a35b2891167a106d427815ad7378f25&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1")
+    @GET("services/rest/?method=flickr.photos.search&api_key=1a34bdcc540e105b49dac5f7e270bf4f&extras=url_s&per_page=20&page=1&format=json&nojsoncallback=1")
     Call<Response> getSearchPhotos(@Query("text") String keyWord);
 
 }
